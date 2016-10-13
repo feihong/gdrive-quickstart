21b7c4920fb69d416610c08b52aba92bf164ab23
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-13 12:01:31 -0500

    Use service account key instead, list all folders

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Python Quickstart: https://developers.google.com/drive/v3/web/quickstart/python
 
 PyDrive documentation: https://github.com/googledrive/PyDrive
 
+## Authentication
+
+Go to [Google developer console](https://console.developers.google.com/), and follow these [directions](http://gspread.readthedocs.org/en/latest/oauth2.html).
+
+Add the email address from the client_email field of the service account key file to the list of users who have permission to access a GDrive folder.
+
 ## Installation
 
 ```
--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,22 @@
+import httplib2
+from oauth2client.service_account import ServiceAccountCredentials
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+
+scopes = ['https://www.googleapis.com/auth/drive.readonly']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'service_account_credentials.json', scopes)
+credentials.authorize(httplib2.Http())
+print(credentials)
+
+
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+gauth.credentials = credentials
 
 drive = GoogleDrive(gauth)
+
+query = "mimeType = 'application/vnd.google-apps.folder'"
+file_list = drive.ListFile({'q': query}).GetList()
+for file1 in file_list:
+  print('title: %s, id: %s' % (file1['title'], file1['id']))
